099ab6331c9254e40056006e3c6810c19f054b7c
Noah Sochandamandon <noah.sochandamandon@gmail.com>
2024-01-18 13:17:26 +0100

    tiles sprites

--- src/endPoint.py
+++ src/endPoint.py
@@ -5,7 +5,13 @@ class End_Point(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
         self.image = pygame.Surface((size, size))
-        self.image.fill((0, 0, 139))
+        self.image.blit(
+            pygame.transform.scale(
+                pygame.image.load("./sprites/block/win.png"),
+                (size, size),
+            ),
+            (0, 0),
+        )
         self.rect = self.image.get_rect(topleft=pos)
 
     def update(self, x_shift):
--- src/tiles.py
+++ src/tiles.py
@@ -1,11 +1,18 @@
 import pygame
 
-class Tuile(pygame.sprite.Sprite):
-    def __init__(self,pos,size):
-        super().__init__()
-        self.image = pygame.Surface((size,size))
-        self.image.fill('grey')
-        self.rect = self.image.get_rect(topleft = pos)
 
-    def update(self,x_shift):
+class Tuile(pygame.sprite.Sprite):
+    def __init__(self, pos, size):
+        super().__init__()
+        self.image = pygame.Surface((size, size))
+        self.image.blit(
+            pygame.transform.scale(
+                pygame.image.load("./sprites/block/block.png"),
+                (size, size),
+            ),
+            (0, 0),
+        )
+        self.rect = self.image.get_rect(topleft=pos)
+
+    def update(self, x_shift):
         self.rect.x += x_shift
