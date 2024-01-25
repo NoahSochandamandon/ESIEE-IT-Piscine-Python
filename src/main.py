5e7b094d2da10850bc1615137e60593c211f60c7
Noah Sochandamandon <noah.sochandamandon@gmail.com>
2024-01-25 19:44:31 +0100

    comment main and modèle

--- src/main.py
+++ src/main.py
@@ -5,12 +5,18 @@ from controleur import JeuControleur
 
 
 def main():
+    # Création des instances du modèle, de la vue, et du controleur
     MODELE = JeuModel()
     VUE = JeuVue()
     CONTROLEUR = JeuControleur(MODELE, VUE)
+
+    # Lancement du jeu à travers le controleur
     CONTROLEUR.jouer()
+
+    # Fermeture pygame à la fin du jeu
     pygame.quit()
 
 
+# Vérification pour exécuter le jeu uniquement si ce script est le point d'entrée principal
 if __name__ == "__main__":
     main()
--- src/modele.py
+++ src/modele.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+# constantes globals qui permettent le paramètrage de base du jeu
 TILE_SIZE = 64
 LONGUEUR = 1920
 LARGEUR = 1080
@@ -10,19 +11,20 @@ GAME_NAME = "L'envolée Educative"
 
 class JeuModel:
     def __init__(self):
+        # changer ici si il y a ajout de modèle
         self.LVL_MAX = 2
         self.vivant = False
 
         self.ecran = 1  # 1 = démarrage, 2 = selection niveaux, 3 = mort, 4 = victoire, 5 = lvl 1, 6 = lvl 2
         self.selected_lvl = 1  # variable de selection de niveau
         self.selected_death_box = 1  # variable qui permet de connaitre la text box selectionné dans la page de mort et de victoire
-        self.score = 0
 
     # setter and getter
     def get_selected_lvl(self):
         return self.selected_lvl
 
     def set_selected_lvl(self, value):
+        # calcul pour avoir le bouton quitter
         if value > self.LVL_MAX + 1:
             self.selected_lvl = 1
         elif value < 1:
@@ -37,6 +39,7 @@ class JeuModel:
         return self.selected_death_box
 
     def set_selected_death_box(self, value):
+        # calcul pour avoir le bouton quitter
         if value > 3:
             self.selected_death_box = 1
         elif value < 1:
@@ -56,13 +59,5 @@ class JeuModel:
     def set_vivant(self, value):
         self.vivant = value
 
-    # fonction utiles au jeu
-    def ajouter_obstacle(self):
-        # Ajoute un nouvel obstacle à une position aléatoire
-        x = random.randint(0, 600)
-        y = random.randint(0, 400)
-        self.obstacles.append([x, y])
-
     def mise_a_jour(self):
-        # Met à jour le jeu (par exemple, déplacer les obstacles, augmenter le score)
-        self.score += 1
+        return
