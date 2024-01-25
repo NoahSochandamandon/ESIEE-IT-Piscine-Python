2790740bb992f31b2a842007654aa16a737f98d8
Noah Sochandamandon <noah.sochandamandon@gmail.com>
2024-01-25 20:24:01 +0100

    comment vue and controleur

--- src/controleur.py
+++ src/controleur.py
@@ -4,9 +4,12 @@ import pygame
 
 class JeuControleur:
     def __init__(self, modele, vue):
+        # références au modèle et à la vue du jeu
         self.modele = modele
         self.vue = vue
+        # indicateur pour contrôler la boucle du jeu
         self.play = True
+        # Dictionnaire pour suivre l'état des touches pressées
         self.touche_enfoncee = {
             pygame.K_LEFT: False,
             pygame.K_RIGHT: False,
@@ -14,10 +17,12 @@ class JeuControleur:
             pygame.K_DOWN: False,
             pygame.K_SPACE: False,
         }
+        # Horloge pour contrôler les mises à jour du jeu
         self.clock = pygame.time.Clock()
 
     def traiter_evenements(self):
         for event in pygame.event.get():
+            # Traitement pour le dictionnaire de touche enfoncée ou non
             if event.type == pygame.QUIT:
                 self.play = False
             elif event.type == pygame.KEYDOWN and (self.modele.get_ecran() > 4):
@@ -27,7 +32,7 @@ class JeuControleur:
                 if event.key in self.touche_enfoncee:
                     self.touche_enfoncee[event.key] = False
 
-            # event des menus
+            # Traitement des événements pour les différents écrans de menu
             elif event.type == pygame.KEYDOWN:
                 # écran de selection des niveaux
                 if event.key == pygame.K_UP and self.modele.get_ecran() == 2:
@@ -53,6 +58,8 @@ class JeuControleur:
                 elif event.key == pygame.K_SPACE and self.modele.get_ecran() == 1:
                     self.modele.set_ecran(2)
 
+                # Gestion des actions sur les écrans de mort et de victoire
+                # ... (logique pour gérer les actions comme recommencer le niveau, retourner au menu principal, etc.)
                 # ecran de mort
                 elif event.key == pygame.K_UP and self.modele.get_ecran() == 3:
                     self.modele.set_selected_death_box(
@@ -115,11 +122,13 @@ class JeuControleur:
 
     def jouer(self):
         while self.play:
+            # Appel de la méthode pour gérer les événements
             self.traiter_evenements()
 
             # event de jeu
             self.modele.mise_a_jour()
 
+            # Affichage de l'écran selectionné
             if self.modele.get_ecran() == 1:
                 self.vue.afficher_screen_1(self.modele)
             elif self.modele.get_ecran() == 2:
@@ -134,6 +143,9 @@ class JeuControleur:
                 self.vue.afficher_screen_6(self.modele)
 
             pygame.display.update()
+
+            # garde le jeu à 60 fps
             self.clock.tick(60)
+        # quitte le jeu quand la boucle est finie
         pygame.quit()
         sys.exit()
--- src/vue.py
+++ src/vue.py
@@ -1,3 +1,4 @@
+# importation des niveau et de la data nécéssaire à la création des vues
 import pygame
 from level import Niveau
 from map.map import *
@@ -8,25 +9,32 @@ import os
 class JeuVue:
     def __init__(self):
         pygame.init()
+        # création de la fenêtre de jeu
         self.ecran = pygame.display.set_mode((LONGUEUR, LARGEUR))
         self.fond = pygame.transform.scale(
             pygame.image.load("./sprites/background/wall.jpg"), (LONGUEUR, LARGEUR)
         )
+        # configuration du texte
         self.police = pygame.font.Font(FONT, 36)
         self.couleur_texte = (255, 255, 255)
+
         self.image_lvl = pygame.image.load("./sprites/background/game_background.jpg")
 
+        # initialisation des paramètre pour la music
         pygame.mixer.init()
         pygame.mixer.music.set_volume(0.05)
 
     # vue des menus : screen 1 et 2
     def afficher_screen_1(self, modele):
+        # Affiche l'image de fond sur l'écran et donne le titre à la fenêtre de jeu
         self.ecran.blit(self.fond, (0, 0))
         pygame.display.set_caption(GAME_NAME)
 
+        # affiche un texte en lui donnant la police et la couleur défini
         texte = "Appuyez sur espace pour commencer"
 
         surface_texte = self.police.render(texte, True, self.couleur_texte)
+        # potionne le texte au centre de l'écran
         position_texte = surface_texte.get_rect(
             center=(LONGUEUR / 2, LARGEUR / 2 + 100)
         )
@@ -36,20 +44,24 @@ class JeuVue:
         pygame.display.update()
 
     def afficher_screen_2(self, modele):
+        # Mise à jour de l'image de fond pour la sélection de niveau
         self.fond = pygame.transform.scale(
             pygame.image.load("./sprites/background/wall.jpg"), (LONGUEUR, LARGEUR)
         )
         self.ecran.blit(self.fond, (0, 0))
 
+        # Création des boîtes de texte pour la selection de niveau
         text_boxes = [
             self.creer_text_box("niveau 1", LONGUEUR // 2, LARGEUR // 2 - 50, 300, 60),
             self.creer_text_box("niveau 2", LONGUEUR // 2, LARGEUR // 2 + 50, 300, 60),
             self.creer_text_box("Quitter", LONGUEUR // 2, LARGEUR // 2 + 250, 250, 60),
         ]
 
+        # Affichage des boîtes de texte à l'écran
         for index, (rect, text_surface) in enumerate(text_boxes):
             pygame.draw.rect(self.ecran, (60, 60, 60), rect)
             rect_contour = rect.inflate(10, 10)
+            # Met en évidence la boîte sélectionnée
             if (index + 1) == modele.get_selected_lvl():
                 pygame.draw.rect(self.ecran, (255, 255, 255), rect_contour, 8)
             self.ecran.blit(
@@ -65,6 +77,8 @@ class JeuVue:
     # vue des niveau in game
     def afficher_screen_5(self, modele):
         self.ecran.blit(self.image_lvl, (0, 0))
+
+        # vérifie si le joueur est vivant ou non pour initialisé le niveau
         if not modele.get_vivant():
             self.initialiser_niveau(carte_niveau_1)
             modele.set_vivant(True)
@@ -73,6 +87,7 @@ class JeuVue:
         if not self.level.get_player_life():
             self.player_dead(modele)
 
+        # fonction de la victoire du joueur
         if self.level.get_victory():
             self.player_win(modele)
 
@@ -80,6 +95,8 @@ class JeuVue:
 
     def afficher_screen_6(self, modele):
         self.ecran.blit(self.image_lvl, (0, 0))
+
+        # vérifie si le joueur est vivant ou non pour initialisé le niveau
         if not modele.get_vivant():
             self.initialiser_niveau(carte_niveau_2)
             modele.set_vivant(True)
@@ -105,6 +122,7 @@ class JeuVue:
 
         texte = "Ca sent le redoublement ici"
 
+        # comme pour la selection des niveaux, génération de textes box et placement pour les choix à la mort du joueur
         text_boxes = [
             self.creer_text_box("Reessayer", LONGUEUR // 2, LARGEUR // 2 + 50, 300, 60),
             self.creer_text_box(
@@ -151,6 +169,7 @@ class JeuVue:
 
         texte = "Respect pour ton diplome"
 
+        # comme pour la selection des niveaux, génération de textes box et placement pour les choix à la mort du joueur
         text_boxes = [
             self.creer_text_box("Reessayer", LONGUEUR // 2, LARGEUR // 2 + 50, 300, 60),
             self.creer_text_box(
@@ -185,20 +204,24 @@ class JeuVue:
 
         pygame.display.update()
 
+    # fonction qui initialise le niveau
     def initialiser_niveau(self, carte):
         self.level = Niveau(carte, self.ecran)
 
+    # fonction qui permet la création des textes box utiliser quand il y a une selection à réaliser
     def creer_text_box(self, texte, x, y, largeur, hauteur):
         rect = pygame.Rect(x - (largeur / 2), y, largeur, hauteur)
         text_surface = self.police.render(texte, True, self.couleur_texte)
         return rect, text_surface
 
+    # lance la music quand aucun son ne doit se supperposé
     def launch_music(self, root):
         pygame.mixer.music.stop()
         loading_file = ".\\music\\" + root
         pygame.mixer.music.load(loading_file)
         pygame.mixer.music.play(loops=0)
 
+    # lance la music quand 2 son doivent se supperposé
     def launch_music_duo(self, root, root2):
         pygame.mixer.music.stop()
         loading_file = ".\\music\\" + root
@@ -207,11 +230,13 @@ class JeuVue:
         pygame.mixer.music.load(loading_file)
         pygame.mixer.music.play(loops=0)
 
+    # fonction de la mort
     def player_dead(self, modele):
         modele.set_ecran(3)
         self.launch_music("loose.mp3")
         modele.set_vivant(False)
 
+    # fonction de la victoire
     def player_win(self, modele):
         modele.set_ecran(4)
         self.launch_music("victory.mp3")
