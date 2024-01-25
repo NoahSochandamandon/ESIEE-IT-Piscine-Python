cf6fe7c7516561f2654b57dc598c152fe37a57a7
Noah Sochandamandon <noah.sochandamandon@gmail.com>
2024-01-25 21:30:25 +0100

    endpoint and tiles comment

--- src/endPoint.py
+++ src/endPoint.py
@@ -4,6 +4,7 @@ import pygame
 class End_Point(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
+        # Création de l'image du point de fin
         self.image = pygame.Surface((size, size))
         self.image.blit(
             pygame.transform.scale(
@@ -12,7 +13,9 @@ class End_Point(pygame.sprite.Sprite):
             ),
             (0, 0),
         )
+        # Création du rectangle pour la position et les collisions
         self.rect = self.image.get_rect(topleft=pos)
 
+    # Méthode pour mettre à jour la position du point de fin
     def update(self, x_shift):
         self.rect.x += x_shift
--- src/tiles.py
+++ src/tiles.py
@@ -4,6 +4,7 @@ import pygame
 class Tuile(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
+        # Création de l'image de la tuile
         self.image = pygame.Surface((size, size))
         self.image.blit(
             pygame.transform.scale(
@@ -12,7 +13,9 @@ class Tuile(pygame.sprite.Sprite):
             ),
             (0, 0),
         )
+        # Création du rectangle pour la position et les collisions
         self.rect = self.image.get_rect(topleft=pos)
 
+    # Méthode pour mettre à jour la position de la tuile
     def update(self, x_shift):
         self.rect.x += x_shift
